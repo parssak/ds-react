6b581a5b225143a1be8a03e65424b4149817a9e3
Parssa Kyanzadeh <parssak@gmail.com>
2022-02-17 12:16:07 -0500

    finished basic example

--- src/pages/sticky.tsx
+++ src/pages/sticky.tsx
@@ -1,34 +1,96 @@
 import React, { useState, useEffect } from "react";
 
-const Note = ({
-  id,
-  title,
-  content,
-  onDelete,
-  onEdit,
-}: {
+interface INote {
   id: string;
   title: string;
   content: string;
+}
+
+const Note = ({
+  note,
+  onDelete,
+  onEdit,
+}: {
+  note: INote;
   onDelete: () => void;
-  onEdit: () => void;
+  onEdit: (attribute: string, content: string) => void;
 }) => {
+  const onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onEdit("title", e.target.value);
+  };
+  const onContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onEdit("content", e.target.value);
+  };
+
   return (
-    <div className="flex flex-col space-y-4">
-      <div className="flex items-center justify-between">
-        <h1>{title}</h1>
-        <button onClick={onDelete}>Delete</button>
-        <textarea onChange={onEdit} value={content} />
+    <div className="bg-yellow-50 p-2">
+      <div className="flex space-x-2 mb-4">
+        <input
+          className="w-full bg-transparent"
+          type="text"
+          value={note.title}
+          onChange={onTitleChange}
+        />
+        <button onClick={onDelete} className="text-sm">
+          Delete
+        </button>
       </div>
+      <textarea
+        className="w-full bg-yellow-100 bg-opacity-50"
+        placeholder="Add content here..."
+        value={note.content}
+        onChange={onContentChange}
+      />
     </div>
   );
 };
 
 export default function StickyPage() {
+  const [notes, setNotes] = useState<INote[]>([]);
+
+  const addNote = () => {
+    setNotes((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: "New Note",
+        content: "",
+      },
+    ]);
+  };
+
+  const editNote = (id: string, attribute: string, value: string) => {
+    setNotes((prev) =>
+      prev.map((note) => {
+        if (note.id === id) {
+          console.debug("editNote", note, attribute, value);
+          return { ...note, [attribute]: value };
+        }
+        return note;
+      })
+    );
+  };
+
+  const deleteNote = (id: string) => {
+    setNotes((prev) => prev.filter((note) => note.id !== id));
+  };
+
   return (
     <div className="container min-h-screen pt-8">
       <h1 className="text-2xl font-bold mb-4">Sticky Notes</h1>
-      <div className="h-48 rounded-xl grid place-items-center bg-yellow-100 text-yellow-800 font-bold">Still need to do this one</div>
+      <button className="btn" onClick={addNote}>
+        New Note +{" "}
+      </button>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5 mt-4">
+        {notes.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            onDelete={() => deleteNote(note.id)}
+            onEdit={(attribute, value) => editNote(note.id, attribute, value)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -54,7 +54,7 @@ const TodoList = ({
           onKeyDown={(e) => e.key === "Enter" && addTodo()}
         />
         <button
-          className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 whitespace-nowrap"
+          className="btn"
           onClick={addTodo}
         >
           Add Task
